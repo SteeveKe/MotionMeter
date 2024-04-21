8c2efcdeeefc3f8c247242517a3725fc65cac5a6
SteeveKe <SteeveKefr@gmail.com>
2024-04-20 19:56:56 -0700

    added admin1 and usertest1

--- MotionMeter/app/src/main/java/com/capucinetulipe/motionmeter/database/UserDAO.java
+++ MotionMeter/app/src/main/java/com/capucinetulipe/motionmeter/database/UserDAO.java
@@ -1,6 +1,7 @@
 package com.capucinetulipe.motionmeter.database;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -12,8 +13,14 @@ import java.util.List;
 @Dao
 public interface UserDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insert(User user);
+    void insert(User... user);
 
-    @Query("Select * from " + MotionMeterDatabase.userTable)
-    List<User> getAllRecords();
+    @Delete
+    void delete(User user);
+
+    @Query("Select * from " + MotionMeterDatabase.USER_TABLE + " ORDER BY username")
+    List<User> getAllUsers();
+
+    @Query("DELETE from " + MotionMeterDatabase.USER_TABLE)
+    void deleteAll();
 }
